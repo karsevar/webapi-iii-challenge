de421fd9d784426a7be586d842cbdd3c2c9f5f08
masonkarsevar <masonkarsevar@gmail.com>
2019-09-04 13:35:24 -0700

    delete functionality is finished with posts route

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -15,12 +15,24 @@ router.get('/', (req, res) => {
         })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validatePostId, (req, res) => {
+    postDb.getById(req.post.id) 
+        .then(results => {
+            res.status(201).json(results)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validatePostId, (req, res) => {
+    postDb.remove(req.post.id) 
+        .then(results => {
+            res.status(200).json({statusCode: results})
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
 });
 
 router.put('/:id', (req, res) => {
@@ -35,12 +47,24 @@ function validatePostId(req, res, next) {
     postDb.getById(postId) 
         .then(results => {
             if (results === undefined) {
-                res.status(400).json({ message: "invalid user id" })
+                res.status(400).json({ message: "invalid post id" })
             } else {
                 req.post = results
                 next();
             }
         })
+};
+
+function validatePost(req, res, next) {
+    if (!Object.keys(req.body).length) {
+        res.status(400).json({ message: "missing post data" })
+    } else {
+        if (req.body.text) {
+            next();
+        } else {
+            res.status(400).json({ message: "missing required text field" })
+        }
+    }
 };
 
 module.exports = router;
