bc09b701a8bd9457eab2a1cfb4daa40282d32890
masonkarsevar <masonkarsevar@gmail.com>
2019-09-04 13:27:44 -0700

    basic root get request is working with the posts route

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,9 +1,18 @@
-const express = 'express';
+const express = require('express');
+const postDb = require('./postDb.js')
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.use(express.json());
 
+router.get('/', (req, res) => {
+    postDb.get()
+        .then(results => {
+            res.status(201).json(results)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
 });
 
 router.get('/:id', (req, res) => {
@@ -21,7 +30,17 @@ router.put('/:id', (req, res) => {
 // custom middleware
 
 function validatePostId(req, res, next) {
+    const postId = req.params.id;
 
+    postDb.getById(postId) 
+        .then(results => {
+            if (results === undefined) {
+                res.status(400).json({ message: "invalid user id" })
+            } else {
+                req.post = results
+                next();
+            }
+        })
 };
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const userRouter = require('./users/userRouter.js')
-
+const userRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter.js');
 const server = express();
 
 server.use('/users', userRouter);
-
+server.use('/posts', postRouter);
 server.use('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
